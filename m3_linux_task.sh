b0c3f9a7bfff0d1dae133f209756696e856c55ea
alievilya <aliev.ilia@yandex.ru>
2016-11-18 21:10:50 +0300

    Update m3_linux_task.sh

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,1 +1,5 @@
 wget -q -O - http://neva.today grep -o "http:.*" | grep -E -v "http://neva.today|http://neva.today" > nevatoday.txt
+
+ -dump -listonly "class="main_one_news"" | grep -o "http:.*" | grep -E -v "http://neva.today|http://neva.today" > file.txt
+ 
+ wget -q -O - class="main_one_news" grep -o "http:.*" | grep -E -v "http://neva.today|http://neva.today" > nevatoday.txt
