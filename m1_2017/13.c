cdabcdcf139a9a9a233ef08f2d46164db670b9c2
alievilya <aliev.ilia@yandex.ru>
2017-03-22 22:52:18 +0300

    Create 13.c

---
+++ m1_2017/13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(void)
+{
+	int m = -1, i, b[1000], j = 0, k = 0, x;
+	char c[1000];
+	gets(c);
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] == ' ')
+		{
+				b[j] = i;
+				j++;
+				k = i;
+				m = 0;
+		}
+		else
+			if (c[i + 1] == '\0')
+			{
+				b[j] = i - k - m;
+				j++;
+			}
+	}
+	for (i = 0; i<j; i++)
+	{
+		for (x = 0; x<b[i]; x++)
+			printf("=");
+		printf("\n\n");
+	}
+
+}
