8130d7551751886f588141cb60259e13a422e991
alievilya <aliev.ilia@yandex.ru>
2017-03-22 22:32:19 +0300

    Create 5.c

---
+++ m1_2017/5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+void main(void)
+{
+	setlocale(LC_CTYPE, "Russian");
+	float bot, top, f;
+
+	printf("Введите нижнюю границу интервала : \n");
+	scanf_s("%f", &bot);
+	printf("Введите верхнюю границу интервала : \n");
+	scanf_s("%f", &top);
+	printf("      C      |      F      \n"
+		"_____________|_____________\n");
+	if (bot <= top)
+	{
+	
+		for (float i = bot; i <= top; i++)
+		{
+			f = 1.8*bot + 32;
+			printf("%12f | %12f\n", bot, f);
+			bot += 1;
+		} 
+			
+	}
+	else
+	{
+		printf("Введите правильную границу интервала \n\n");
+		
+	}
+}
