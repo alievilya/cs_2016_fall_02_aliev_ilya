13b2e1c3ec63d2075a1cb048f193839780e645d5
alievilya <aliev.ilia@yandex.ru>
2017-03-24 22:51:14 +0400

    Update 3.C

--- m1_2017/3.C
+++ m1_2017/3.C
@@ -5,7 +5,7 @@ void main(void)
 
 	float x, y, a, b, c, d;
 	printf("Enter numbers a+bi,x+yi\n ");
-	scanf_s("%f+%fi,%f+%fi", &x, &y, &a, &b);
+	scanf("%f+%fi,%f+%fi", &x, &y, &a, &b);
 	c = (x*x*a + x*a*a + x*b*b + a*y*y) / ((x + a)*(x + a) + (y + b)*(y + b));
 	d = (x*x*b + a*a*y + y*y*b + y*b*b) / ((x + a)*(x + a) + (y + b)*(y + b));
 	printf("(%.3f)+(%.3fi)\n", c, d);
