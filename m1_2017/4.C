ec0bbeb815a7dcdfe71011d932f01df33596e53c
alievilya <aliev.ilia@yandex.ru>
2017-03-24 23:01:00 +0400

    Update 4.C

--- m1_2017/4.C
+++ m1_2017/4.C
@@ -4,8 +4,8 @@ void main(void)
 {
 	int x, y;
 	printf("enter x, y\n");
-	scanf_s("%d %d", &x, &y);
+	scanf("%d %d", &x, &y);
 	swap1(int, x, y);
 	printf("%d %d\n", x, y);
-	switch (_getch());
+	
 }
