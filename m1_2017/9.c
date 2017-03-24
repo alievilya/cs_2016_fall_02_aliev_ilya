3c182c19f3c167b28ae370d019706d2b4f5d9645
alievilya <aliev.ilia@yandex.ru>
2017-03-24 23:49:18 +0400

    Update 9.c

--- m1_2017/9.c
+++ m1_2017/9.c
@@ -1,8 +1,7 @@
 #include "stdio.h"
-#include "conio.h"
 
 
-int fib(int n)
+int fib(long int n)
 {
 	if (n == 0) { return 0; }
 	else if ((n == -1) || (n == 1)) { return 1; }
@@ -19,9 +18,9 @@ void main(void)
 
 	long int n;
 	printf("Input n: ");
-	scanf_s("%d", &n);
+	scanf("%ld", &n);
 	for (int i = 0; i <= n; i++)
 
 		printf("%d - %d\n", i, fib(i));
-	switch (_getch());
+
 }
