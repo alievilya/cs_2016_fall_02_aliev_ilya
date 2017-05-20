4cd728df6c1d8d2c7950ff964cb6a016b13359af
alievilya <aliev.ilia@yandex.ru>
2017-05-20 14:35:47 +0300

    changes in fuctions

--- m2_2017/differ.c
+++ m2_2017/differ.c
@@ -4,33 +4,36 @@
 
 float func(float x)
 {
-	return x*(x+1);
+	return x*x*x;
 }
 
 void main()
 {
 	float step = 0.0, x = 0.0, low=0.0, high=0.0;
-	float P = 0.0, P1=0.0, P2=0.0;
+	float F = 0.0, F1=0.0, F2=0.0;
 	int i;
 
-	printf("Enter low:\n");
+	printf("Please Enter low:\n");
 	scanf("%f", &low);
-	printf("Enter high:\n");
+	
+	printf("Please Enter high:\n");
 	scanf("%f", &high);
-	printf("Enter step:\n");
+	
+	printf("Please Enter step:\n");
 	scanf("%f", &step);
 	x = low;
-	P1 = ((-3) * func(x) + 4 * (func(x+step)) - (func(x+2*step)))/(2*step);
-	P2 = (1 / (2 * step))*(-func(x) + func(x + 2 * step));
-	printf("%f\n", P1);
-	printf("%f\n", P2);
-	do
+	F1 = ((-3) * func(x) + 4 * (func(x+step)) - (func(x+2*step)))/(2*step);
+	F2 = (1 / (2 * step))*(-func(x) + func(x + 2 * step));
+	printf("%f\n", F1);
+	printf("%f\n", F2);
+	while  (x+2*step <= high)
 	{
-		P = (1 / (2 * step))*(func(x) - 4 * (func(x + step))+3*(func(x+2*step)));
+		F = (1 / (2 * step))*(func(x) - 4 * (func(x + step))+3*(func(x+2*step)));
+		
+		printf("%f\n", F);
 		x = x + step;
-		printf("%f\n", P);
 	}
-	while  (x+2*step <= high);
+	
 
 
 }
--- m2_2017/integrirovanie.c
+++ m2_2017/integrirovanie.c
@@ -3,13 +3,13 @@
 
 float func(float x)
 {
-	return x*x;
+	return x*(x+1);
 }
 
-int main()
+void main()
 {
-    float low=0.0, high=0.0 , j = 0.0, n=0.0, x = 0.0, p = 0.0;
-	float p1=0.0, p2=0.0, res0=0.0, res1=0.0, i=0.0, k=1.0;
+    float low=0.0, high=0.0 , j = 0.0, n=0.0, x = 0.0, F = 0.0;
+	float F1=0.0, F2=0.0, res0=0.0, res1=0.0, i=0.0, k=1.0;
     printf("Enter low:\n");
 	scanf("%f", &low);
 	printf("Enter high:\n");
@@ -17,17 +17,18 @@ int main()
 	printf("Enter n:\n");
 	scanf("%f", &n);
     i=(high-low)/(2*n);
-    j=(high-low)/(2*n);
-    p=(j/3)*(func(low)+func(high));
+    j=i;
+    F=(j/3)*(func(low)+func(high));
+    
 	while(high > low+2*k*i || high>low+(2*k+1)*i)
     {
-        p1=(2*j/3)*(func(low+2*k*i));
-        p2=(4*j/3)*(func(low+i*(2*k+1)));
+        F1=(2*j/3)*(func(low+2*k*i));
+        F2=(4*j/3)*(func(low+i*(2*k+1)));
         k=k+1.0;
-        res0=res0+p1+p2;
+        res0=res0+F1+F2;
 	}
-	res1=p+res0;
-	printf("%f\n", res1);
 
-    return 0;
+	printf("%f\n", F+res0);
+
+
 }
